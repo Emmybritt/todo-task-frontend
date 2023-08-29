86582f6b671f78a3dcc7212cd0b909683c271744
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2023-08-29 17:27:41 +0100

    add prod baseurl

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const myAxios = axios.create({
-	// baseURL: "https://todo-node-api.onrender.com/api/v1",
-	baseURL: "http://localhost:3333/api/v1",
+	baseURL: "https://todo-node-api.onrender.com/api/v1",
+	// baseURL: "http://localhost:3333/api/v1",
 	withCredentials: true,
 });
